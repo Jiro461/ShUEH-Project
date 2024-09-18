6361a3b6a2cdd7587e450556914942fb700103aa
MecZaWy <huymachgia555@gmail.com>
2024-09-18 20:34:35 +0700

    Initialize Project FE & BE

---
+++ BackEnd-ASP.NET/Program.cs
@@ -0,0 +1,58 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000") // Địa chỉ của frontend
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Enable CORS
+app.UseCors("AllowSpecificOrigins"); //Sử dụng Cors
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
---
+++ FrontEnd-ReactJS/webshoes/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [forecast, setForecast] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        const response = await fetch('http://localhost:5118/weatherforecast'); // Chỉnh đúng URL API của bạn
+        const data = await response.json();
+        setForecast(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchWeather();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather Forecast</h1>
+        <ul>
+          {forecast.map((weather, index) => (
+            <li key={index}>
+              <p>Date: {weather.date}</p>
+              <p>Temperature: {weather.temperatureC}°C ({weather.temperatureF}°F)</p>
+              <p>Summary: {weather.summary}</p>
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
