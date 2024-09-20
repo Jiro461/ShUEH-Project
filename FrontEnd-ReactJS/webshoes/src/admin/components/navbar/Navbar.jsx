3115687b1ad4dd40c4ee20f0df541b9a22f9c8d3
Jiro <huymachgia555@gmail.com>
2024-09-20 20:03:47 +0700

    Add Image to Public

--- FrontEnd-ReactJS/webshoes/src/admin/components/navbar/Navbar.jsx
+++ FrontEnd-ReactJS/webshoes/src/admin/components/navbar/Navbar.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+const IMAGE_PATH = '../../../../public'
 const Navbar = () => {
     var myvalue = process.env.REACT_APP_API_URL;
+    var IMAGE_PATH = process.env.PUBLIC_URL;
     return (
         <div className='navbar'>
             <div className='logo'>
-                <img src='logo.svg' alt =""/>
+                <img src='logo.svg' alt="" />
                 <span>{myvalue}</span>
             </div>
             <div className='icons'>
-                <img src='' alt="" className='icon'/>
-                <img src="" alt="" className='icon'/>
-                <img src="" alt="" className='icon'/>
+                <img src={`/search.svg`} alt="" className='icon' />
+                <img src={`/app.svg`} alt="" className='icon' />
+                <img src={`/expand.svg`} alt="" className='icon' />
                 <div className="notification"></div>
                 <div className="user"></div>
-                <img src="" alt="" className='icon'/>
+                <img src="" alt="" className='icon' />
             </div>
         </div>
     );
 };
 
 Navbar.propTypes = {
-    
+
 };
 
 export default Navbar;
