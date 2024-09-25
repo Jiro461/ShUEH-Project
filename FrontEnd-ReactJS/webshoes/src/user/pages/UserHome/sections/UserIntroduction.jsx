712565011b99ec36ae4cbce53eeb1388c7b239b4
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-09-25 15:50:48 +0700

    Create introduction section in home page

---
+++ FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserIntroduction.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserIntroduction = () => {
+    return (
+        <>
+            <div>Hello</div>
+        </>
+    );
+};
+
+export default UserIntroduction;
