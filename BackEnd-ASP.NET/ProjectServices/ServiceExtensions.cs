e0761012ea8b71bf8ef4095ce88a69e79e1f7074
MecZaWy <huymachgia555@gmail.com>
2024-10-04 23:07:51 +0700

    Update Service Extension

---
+++ BackEnd-ASP.NET/ProjectServices/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+
+public static class ServiceExtensions
+{
+    /// <summary>
+    /// Đăng ký các dịch vụ (DI) cho ứng dụng.
+    /// </summary>
+    public static void AddProjectServices(this IServiceCollection services)
+    {
+        // Đăng ký các dịch vụ tại đây
+
+
+    }
+}
