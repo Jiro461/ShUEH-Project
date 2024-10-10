69975c04f8c8e47aebcf6184d133a3966f06445e
MecZaWy <huymachgia555@gmail.com>
2024-10-10 15:55:45 +0700

    Program.cs Config

--- BackEnd-ASP.NET/Program.cs
+++ BackEnd-ASP.NET/Program.cs
@@ -1,23 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddProjectServices();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-// Add CORS services
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowSpecificOrigins",
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:3000") // Địa chỉ của frontend
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
+
 
 var app = builder.Build();
+
 app.UseStaticFiles();
 
 if (app.Environment.IsDevelopment())
