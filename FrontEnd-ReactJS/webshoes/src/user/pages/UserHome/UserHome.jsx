19fb8d1580d8369d9957ed3ced2eabe1f1beefd8
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-12 23:20:09 +0700

    Render children section page for home page

--- FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/UserHome.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/UserHome.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import UserIntroduction from './sections/UserIntroduction'
+import UserProducts from "./sections/UserProducts";
+import UserBrandShoes from "./sections/UserBrandShoes";
+import UserMostPopular from "./sections/UserMostPopular";
+import UserAdvantage from "./sections/UserAdvantage";
+import UserCollaboration from "./sections/UserCollaboration";
+import UserReview from "./sections/UserReview";
 
 const UserHome = () => {
     return (
         <>
         <UserIntroduction/>
-        
+        <UserProducts/>
+        <UserBrandShoes/>
+        <UserMostPopular/>
+        <UserAdvantage></UserAdvantage>
+        <UserCollaboration></UserCollaboration>
+        <UserReview></UserReview>
         </>
     );
 };
