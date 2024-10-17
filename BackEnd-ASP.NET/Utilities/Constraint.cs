3fa17202488cb3a1e0014f3854c797b79ffe1e2e
MecZaWy <huymachgia555@gmail.com>
2024-10-18 00:37:06 +0700

    Add Host Constraint

--- BackEnd-ASP.NET/Utilities/Constraint.cs
+++ BackEnd-ASP.NET/Utilities/Constraint.cs
@@ -1,9 +1,18 @@
 using System.Globalization;
 using System.Text.RegularExpressions;
+using BackEnd_ASP.NET.Data;
+using BackEnd_ASP_NET.Models;
+using Microsoft.EntityFrameworkCore;
 namespace BackEnd_ASP_NET.Utilities.Extensions
 {
     public static class MyDateTime
     {
         public static DateTimeOffset VietNam = DateTimeOffset.UtcNow.AddHours(7);
     }
+
+    public static class MyURL
+    {
+        public static string Host = "http://localhost:5118";
+    }
+
 }
