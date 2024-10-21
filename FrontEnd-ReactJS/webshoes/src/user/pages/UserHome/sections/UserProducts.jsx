e49508baa49914c99dcd652d39cfb7d5c3a5f88d
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-21 11:47:22 +0700

    responsive user products home page

--- FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserProducts.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserProducts.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import './UserProducts.scss'
 import Product from "../../../components/product/Product";
+import Slider from 'react-slick'
+import '../../../../../styles/slider.scss'
+import {Reveal} from "../../../components/utils/Reveal.tsx";
 
 const UserProducts = () => {
+    const images = [
+        {id: 2,
+            src: "/product-2.svg"
+        },
+        {id: 3,
+            src: "/product-3.svg"
+        },
+        {id: 4,
+            src: "/product-4.svg"
+        },
+    ]
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
     return (
         <div className="products">
+            <Reveal>
+                <h3>PRODUCTS</h3>
+            </Reveal>
 
-            <h3>PRODUCTS</h3>
-            
-            <div className="navbar">
+            <div className="navbar d-none d-md-flex">
+            <Reveal>
                 <ul>
                     <li>Nike<span>(123)</span></li>
                     <li>Adidas<span>(123)</span></li>
@@ -16,30 +41,44 @@ const UserProducts = () => {
                     <li>Rebok<span>(123)</span></li>
                     <li>Converse<span>(123)</span></li>
                 </ul>
+            </Reveal>
+            <Reveal>
                 <ul>
                     <li>Newest</li>
                     <li>VN size</li>
                     <li>X</li>
                 </ul>
+            </Reveal>
             </div>
 
-            <div className="container-fluid">
+            <div className="container-fluid d-none d-md-block">
                 <div className="row g-0">
-                    <div className="col-4 g-0">
-                        <Product className='img-custom' src="/product-1.svg"></Product>
-                    </div>
+                        <div className="col-4 g-0">
+                    <Reveal>
+                            <Product imgClassName='img-custom' imgSrc="/product-1.svg"></Product>
+                    </Reveal>
+                        </div>
                     <div className="col-8">
                         <div className="row">
-                            <div className="col">
-                                <Product src="/product-2.svg"></Product>
-                            </div>
-                            <div className="col">
-                                <Product src="/product-3.svg"></Product>
-                            </div>
+                                <div className="col">
+                            <Reveal>
+                                    <Product imgSrc="/product-2.svg"></Product>
+                            </Reveal>
+                                </div>
+                                <div className="col">
+                            <Reveal>
+                                    <Product imgSrc="/product-3.svg"></Product>
+                            </Reveal>
+                                </div>
                         </div>
                         <div className="row g-0">
                             <div className="col">
-                                <Product src="/product-4.svg"></Product>
+                                <Reveal>
+                                    <Product imgSrc="/product-4.svg"></Product>
+                                </Reveal>
+                            </div>
+                            <div className="col">
+
                             </div>
                         </div>
                     </div>
@@ -47,18 +86,36 @@ const UserProducts = () => {
 
                 <div className="row g-0">
                     <div className="col">
-                        <Product src="/product-5.svg"></Product>
+                        <Reveal>
+                            <Product imgSrc="/product-5.svg"></Product>
+                        </Reveal>
                     </div>
                     <div className="col">
-                        <Product src="/product-6.svg"></Product>
+                        <Reveal>
+                            <Product imgSrc="/product-6.svg"></Product>
+                        </Reveal>
                     </div>
                     <div className="col">
-                        <Product src="/product-7.svg"></Product>
+                        <Reveal>
+                            <Product imgSrc="/product-7.svg"></Product>
+                        </Reveal>
                     </div>
-                </div>
-                
+                </div>  
+            </div>
+
+            <div className="slider-container d-sm-block d-md-none">
+                <Slider {...settings}>
+                    {images.map((item, index) => {
+                        return <Reveal>
+                            <div key={index} className="slider-item">
+                                <Product width="90%"  imgSrc={item.src}></Product>
+                            </div>
+                        </Reveal>
+                    })}
+                </Slider>
             </div>
         </div>
+        
     );
 };
 
