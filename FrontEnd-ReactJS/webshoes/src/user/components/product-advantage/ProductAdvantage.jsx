9c8b74cded3b08c2303ee7837be45472e8c3f4ab
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-21 11:42:30 +0700

    responsive product advantage component

--- FrontEnd-ReactJS/webshoes/src/user/components/product-advantage/ProductAdvantage.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/components/product-advantage/ProductAdvantage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './ProductAdvantage.scss'
 
-const ProductAdvantage = ({index, name, desc, width, height, pt, bgcolor, flexDirection}) => {
+const ProductAdvantage = ({index, name, desc, src, style}) => {
+    console.log(style?.imgTopPosition); 
     return (
-        <div className="product-advantage" style={{width: width, height: height,backgroundColor: bgcolor, flexDirection: flexDirection}}>
+        <div className="product-advantage" style={{
+            width: style?.width, 
+            height: style?.height, 
+            background: style?.bgcolor,
+            flexDirection: style?.flexDirection}}>
             <span className="advantage-index">{index}</span>
-            {flexDirection ? <span className="advantage-name ml">{name}</span> : <span className="advantage-name">{name}</span>}
+            {style?.flexDirection ? <span className="advantage-name ml">{name}</span> : <span className="advantage-name">{name}</span>}
             {desc ? <span className="advantage-desc">{desc}</span> : "" }
+            {src ? <img style={{
+                position: "absolute",
+                width: `${style?.imgWidth}`,
+                height: `${style?.imgHeight}`,
+                top: `${style?.imgTopPosition}`, 
+                right: `${style?.imgRightPosition}`}} src={src} alt=""></img>: ""}
         </div>
     );
 };
