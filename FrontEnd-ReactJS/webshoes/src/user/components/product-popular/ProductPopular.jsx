c67d57f22069d37dfe60c01196cf6ff1ff21e332
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-21 11:42:02 +0700

    responsive product popular component

--- FrontEnd-ReactJS/webshoes/src/user/components/product-popular/ProductPopular.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/components/product-popular/ProductPopular.jsx
@@ -5,31 +5,36 @@ import PromotionTag from "../promotion-tag/PromotionTag";
 
 const ProductPopular = () => {
     return (
-        <div className="popular-product">
-            <div className="info">
-                <TagNew></TagNew>
-                <span className="info-title">PRODUCT</span>
-                <span className="info-title">NO.###</span>
-                <span className="info-title">300000$</span>
+        <div>
+            <div className="popular-product">
+                <div className="info">
+                    <TagNew></TagNew>
+                    <span className="info-title">PRODUCT</span>
+                    <span className="info-title">NO.###</span>
+                    <span className="info-title">300000$</span>
 
-                <button>Go to Catalog</button>
-            </div>
-            <div className="title">
-                BRAND
-            </div>
-            <div className="product">
-                <img src="/popular-product.svg" alt=""></img>
-                <img className="line-decor-1" src="/popular-line-decor-2.svg" alt=""></img>
-                <div className="promotion-block">
-                    <PromotionTag></PromotionTag>
+                    <button className="d-none d-md-block">Go to Catalog</button>
                 </div>
-                <div className="product-info">
-                    <span className="product-brand">SHUEH</span>
-                    <span className="product-name">OUTDOORS</span>
+                <div className="title">
+                    BRAND
+                </div>
+                <div className="product">
+                    <img className="product-img" src="/popular-product.svg" alt=""></img>
+                    <img className="line-decor-1" src="/popular-line-decor-2.svg" alt=""></img>
+                    <div className="promotion-block">
+                        <PromotionTag></PromotionTag>
+                    </div>
+                    <div className="product-info">
+                        <span className="product-brand">SHUEH</span>
+                        <span className="product-name">OUTDOORS</span>
+                    </div>
+                </div>
+                <div className="line-decor-2">
+                    <img src="/popular-line-decor-1.svg" alt=""></img>
                 </div>
             </div>
-            <div className="line-decor-2">
-                <img src="/popular-line-decor-1.svg" alt=""></img>
+            <div className="btn-block d-flex d-md-none">
+                <button className="btn-custom">Go to Catalog</button>
             </div>
         </div>
     );
