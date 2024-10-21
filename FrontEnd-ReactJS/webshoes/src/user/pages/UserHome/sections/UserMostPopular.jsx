463412cc57447747d1242a47acf92a8bbb2a3179
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-21 11:46:51 +0700

    responsive user most popular home page

--- FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserMostPopular.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserMostPopular.jsx
@@ -2,23 +2,47 @@ import React from 'react';
 import './UserMostPopular.scss'
 import ProductPopular from "../../../components/product-popular/ProductPopular";
 import SectionTitle from "../../../components/section-title/SectionTitle";
+import Slider from "react-slick";
+import '../../../../../styles/slider.scss'
+import { Reveal } from "../../../components/utils/Reveal.tsx";
 
 const UserMostPopular = () => {
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      };
     return (
         <div className="user-most-popular">
             <div className="title">
-                <SectionTitle title="MOST POPULAR"></SectionTitle>
+                <Reveal>
+                    <SectionTitle title="MOST POPULAR"></SectionTitle>
+                </Reveal>
             </div>
-            <div className="product-popular">
-                <div className="sub-left fit-content">
-                    <ProductPopular></ProductPopular>
-                </div>
-                <div className="center-product fit-content">
-                    <ProductPopular></ProductPopular>
-                </div>
-                <div className="sub-right fit-content">
-                    <ProductPopular></ProductPopular>
-                </div>
+            <div className="product-popular d-none d-md-block">
+                    <Reveal>
+                    <div className="sub-left fit-content">
+                        <ProductPopular></ProductPopular>
+                    </div>
+                    <div className="center-product fit-content">
+                        <ProductPopular></ProductPopular>
+                    </div>
+                    <div className="sub-right fit-content">
+                        <ProductPopular></ProductPopular>
+                    </div>
+                </Reveal>
+            </div>
+
+            <div className="slider-container d-md-none d-sm-block">
+               <Slider {...settings}>
+                    <div className="slider-item"><ProductPopular></ProductPopular></div>
+                    <div className="slider-item"><ProductPopular></ProductPopular></div>
+                    <div className="slider-item"><ProductPopular></ProductPopular></div>
+               </Slider>
             </div>
         </div>
     );
