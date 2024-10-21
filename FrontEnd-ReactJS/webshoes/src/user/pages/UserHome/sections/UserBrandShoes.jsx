d41abc83446f2d38b8d713e8074a956b07093de1
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-21 11:46:12 +0700

    responsive user brand shoes home page

--- FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserBrandShoes.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/pages/UserHome/sections/UserBrandShoes.jsx
@@ -6,22 +6,13 @@ import SectionTitle from "../../../components/section-title/SectionTitle";
 const UserBrandShoes = () => {
     return (
         <div className="brand-shoes">
-            <div className="container-fluid">
-                <div className="row g-0">
-                    <div className="col-8">
-                        <div className="row g-0">
-                            <SectionTitle title="BRAND SHOES."></SectionTitle>
-                        </div>
-                        <div className="row g-0">
-                            <div className="col"><Product src="/product-8.svg"></Product></div>
-                            <div className="col"><Product src="/product-9.svg"></Product></div>
-                        </div>
-                    </div>
-                    <div className="col-4">
-                        <Product src="/product-10.svg"></Product>
-                    </div>
-                </div>
-                
+            <SectionTitle className="text-center" title="BRAND SHOES."></SectionTitle>
+            <div className="img-brand-block">
+                <img src="/brand-nike-icon.svg" alt=""></img>
+                <img src="/brand-adidas-icon.svg" alt=""></img>
+                <img src="/brand-puma-icon.svg" alt=""></img>
+                <img src="/brand-reebok-icon.svg" alt=""></img>
+                <img src="/brand-converse-icon.svg" alt=""></img>
             </div>
         </div>
     );
