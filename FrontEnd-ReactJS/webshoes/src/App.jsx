5f0b21b667b212c272e8964a7d0450e6f9c5edc5
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-10-30 17:08:44 +0700

    Insert pages of admin

--- FrontEnd-ReactJS/webshoes/src/App.jsx
+++ FrontEnd-ReactJS/webshoes/src/App.jsx
@@ -3,12 +3,16 @@ import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-d
 import AdminHome from './admin/pages/AdminHome/AdminHome.jsx';
 import AdminLayout from './layouts/admin/AdminLayout.jsx';
 import UserLayout from './layouts/user/UserLayout.jsx';
+import UserHome from './user/pages/UserHome/UserHome.jsx';
 import Users from './admin/pages/Users/Users.jsx';
-import Product from './user/pages/ProductPage/ProductPage.jsx';
+import User from "./admin/pages/User/User.jsx";
+import AdminProducts from "./admin/pages/AdminProducts/AdminProducts.jsx";
+import AdminProduct from "./admin/pages/AdminProduct/AdminProduct.jsx";
+import Orders from "./admin/pages/Orders/Orders.jsx"
+import AdminCalendar from "./admin/pages/Calendar/Calendar.jsx"
 import PaymentPage from './user/pages/PaymentPage/PaymentPage.jsx';
 import ProductDetailPage from './user/pages/ProductDetailPage/ProductDetailPage.jsx';
 import ProfilePage from './user/pages/ProfilePage/ProfilePage.jsx';
-import UserHome from './user/pages/UserHome/UserHome.jsx';
 import './App.scss';
 
 // Định nghĩa component SignInComponent
@@ -350,7 +354,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <SignInComponent />, // Thêm route cho SignInComponent
+        element: <UserHome />, // Thêm route cho SignInComponent
       },
     ],
   },
@@ -365,6 +369,26 @@ const router = createBrowserRouter([
       {
         path: "/admin/users",
         element: <Users />,
+      },
+      {
+        path: "/admin/users/:id",
+        element: <User />,
+      },
+      {
+        path: "/admin/products",
+        element: <AdminProducts />,
+      },
+      {
+        path: "/admin/products/:id",
+        element: <AdminProduct />,
+      },
+      {
+        path: "/admin/orders/",
+        element: <Orders />,
+      },
+      {
+        path: "/admin/calendar/",
+        element: <AdminCalendar />,
       },
     ],
   },
