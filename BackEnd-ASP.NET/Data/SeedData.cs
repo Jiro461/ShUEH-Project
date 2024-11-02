2ba6967db6a9444f9462782f9258f1df1cebd22d
MecZaWy <huymachgia555@gmail.com>
2024-11-02 13:44:31 +0700

    Update SeedDatabase

---
+++ BackEnd-ASP.NET/Data/SeedData.cs
@@ -0,0 +1,22 @@
+// SeedData.cs
+using BackEnd_ASP.NET.Data;
+using BackEnd_ASP_NET.Models;
+using Microsoft.AspNetCore.Identity;
+
+public class SeedData
+{
+    public static async Task Seed(IServiceProvider serviceProvider)
+    {
+        using (var scope = serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<ShUEHContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+            List<Guid> shoeIds = context.Shoes.Select(s => s.Id).ToList();
+            var seeder = new DatabaseSeeder(context, userManager);
+
+            // Gọi phương thức SeedData trong seeder
+            await seeder.SeedData(shoeIds);
+            Console.WriteLine("Seed Data Success");
+        }
+    }
+}
