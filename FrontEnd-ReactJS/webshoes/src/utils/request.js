b28501c4f4dc24cf2addd08388d14d108127dbf3
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-11-04 00:47:40 +0700

    Use axios library with axios instance

--- FrontEnd-ReactJS/webshoes/src/utils/request.js
+++ FrontEnd-ReactJS/webshoes/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://localhost:5118/api/"
+    baseURL: `${process.env.REACT_APP_API_URL}/`
 })
 
 export const get = async (path, options = {}) => {
