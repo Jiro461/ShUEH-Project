1139a06e6ad752ed27737a3999acc4906d4090f1
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-11-09 12:36:51 +0700

    Add discount item to admin menu

--- FrontEnd-ReactJS/webshoes/src/admin/data.jsx
+++ FrontEnd-ReactJS/webshoes/src/admin/data.jsx
@@ -40,6 +40,12 @@
                 url: "/admin/orders/",
                 icon: "order.svg"
             },
+            {
+                id: 4,
+                title: "Discounts",
+                url: "/admin/discounts/",
+                icon: "discount.svg"
+            },
         ]
     },
     {
