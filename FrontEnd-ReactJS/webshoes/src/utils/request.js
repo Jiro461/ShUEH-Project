acd2d0695a4280390bfc11e347b9986390006ca8
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-11-09 12:37:39 +0700

    Config axios instance to get cookie

--- FrontEnd-ReactJS/webshoes/src/utils/request.js
+++ FrontEnd-ReactJS/webshoes/src/utils/request.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/`
+    baseURL: `${process.env.REACT_APP_API_URL}/`,
+    withCredentials: true
 })
 
 export const get = async (path, options = {}) => {
