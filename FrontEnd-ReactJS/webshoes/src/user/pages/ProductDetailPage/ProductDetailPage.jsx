e39db068670c234c04b5f1b375bd863b6b810f26
hoaiphu <rubik1x11@gmail.com>
2024-11-09 12:13:19 +0700

    Fix: ProductDetailPage - sliderSettings

--- FrontEnd-ReactJS/webshoes/src/user/pages/ProductDetailPage/ProductDetailPage.jsx
+++ FrontEnd-ReactJS/webshoes/src/user/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -65,7 +65,7 @@ const ProductDetailPage = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    slidesToShow: 5,
     slidesToScroll: 1,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 3 } },
