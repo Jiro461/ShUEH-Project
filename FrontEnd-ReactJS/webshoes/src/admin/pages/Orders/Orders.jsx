6e7920a33d630d484d2dd3de0919a8db882be9e1
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-11-09 12:35:04 +0700

    Fix admin orders page

--- FrontEnd-ReactJS/webshoes/src/admin/pages/Orders/Orders.jsx
+++ FrontEnd-ReactJS/webshoes/src/admin/pages/Orders/Orders.jsx
@@ -1,93 +1,147 @@
-
 import DataTable from "../../components/dataTable/DataTable";
 import "./Orders.scss";
 import { useEffect, useState } from "react";
-import Add from "../../components/add/Add";
-import { userRows } from "../../data";
-import axios from "axios";
-// import { useQuery } from "@tanstack/react-query";
+import { Alert, Backdrop, CircularProgress, Slide, Snackbar } from "@mui/material";
+import * as adminOrdersService from "../../../services/adminOrdersService"
 
 const columns = [
-  { field: "id", headerName: "ID", width: 90 },
+  { field: "id", headerName: "Order Id", width: 130 },
   {
-    field: "img",
-    headerName: "Avatar",
+    field: "userId",
+    type: "string",
+    headerName: "User Id",
+    width: 110,
+  },
+  {
+    field: "userName",
+    type: "string",
+    headerName: "User Name",
+    width: 110,
+  },
+  {
+    field: "userEmail",
+    type: "string",
+    headerName: "User Email",
+    width: 150,
+  },
+  {
+    field: "totalItems",
+    type: "string",
+    headerName: "Total Items",
+    width: 90,
+  },
+  {
+    field: "totalPrice",
+    type: "string",
+    headerName: "Total Price",
+    width: 90,
+  },
+  {
+    field: "status",
+    type: "string",
+    headerName: "Order Status",
     width: 100,
-    renderCell: (params) => {
-      return <img src={params.row.img || "/noavatar.png"} alt="" />;
-    },
   },
   {
-    field: "firstName",
+    field: "paymentMethod",
     type: "string",
-    headerName: "First name",
-    width: 110,
+    headerName: "Payment",
+    width: 80,
   },
   {
-    field: "lastName",
+    field: "orderDate",
     type: "string",
-    headerName: "Last name",
-    width: 110,
-  },
-  {
-    field: "email",
-    type: "string",
-    headerName: "Email",
-    width: 150,
-  },
-  {
-    field: "phone",
-    type: "string",
-    headerName: "Phone",
-    width: 150,
-  },
-  {
-    field: "createdAt",
-    headerName: "Created At",
-    width: 150,
-    type: "string",
-  },
-  {
-    field: "verified",
-    headerName: "Verified",
-    width: 150,
-    type: "boolean",
+    headerName: "Order At",
+    width: 100,
   },
 ];
+const status = ["Pending", "Confirmed", "Shipped", "Delivered", "Canceled"]
+const inputs =[
+  {
+    field: "status",
+    type: "selectOnly",
+    label: "Status",
+    require: true,
+    selectData: status
+  },
+  ]
 
-const Orders = () => {
-  const [open, setOpen] = useState(false);
+const Orders =  () => {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [openBackDrop, setOpenBackDrop] = useState(false)
+  const [openToastMessage, setOpenToastMessage] = useState(false)
+  const [typeToastMessage, setTypeToastMessage] = useState("")
+  const [titleToastMessage, setTitleToastMessage] = useState("")
 
+  const fetchData = async () => {
+    try {
+      const res = await adminOrdersService.getAllOrders()
+      setData(res); // Lưu dữ liệu vào state
+    } catch (error) {
+      console.log(error);
+    }
+    finally{
+      setIsLoading(false)
+    }
+  };
   // Gọi API trong useEffect
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get("http://localhost:5118/api/Account/get-users-info");
-        setData(res.data); // Lưu dữ liệu vào state
-      } catch (err) {
-        console.error(err); // Xử lý lỗi nếu có
-      } finally{
-        setIsLoading(false)
-      }
-    };
-
     fetchData(); // Gọi hàm fetchData
   }, []); // Chỉ chạy một lần sau khi component mount
+  
+  function SlideTransition(props) {
+    return <Slide {...props} direction="left" />;
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenToastMessage(false);
+  }
   return (
     <div className="orders">
-      <div className="info">
-        <h1>Orders</h1>
-        <button onClick={() => setOpen(true)}>Add New Order</button>
-      </div>
-      {isLoading ? (
-        "Loading..."
-      ) : (
-        <DataTable slug="orders" columns={columns} rows={data} />
-      )}
-      {open && <Add slug="order" columns={columns} setOpen={setOpen} />}
+    <div className="info">
+      <h1>Orders</h1>
     </div>
+    {isLoading ? (
+      <CircularProgress color="inherit" />
+    ) : (
+      <DataTable 
+      slug="orders" 
+      updateSlug="order"
+      columns={columns} 
+      rows={data}
+      inputs={inputs} 
+      fetchData={fetchData}
+      setOpenBackDrop={setOpenBackDrop}
+      setOpenToastMessage={setOpenToastMessage}
+      setTypeToastMessage={setTypeToastMessage}
+      setTitleToastMessage={setTitleToastMessage}
+      />
+    )}
+
+    {openBackDrop && (<Backdrop
+        sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+        open={openBackDrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>)}
+   
+    {openToastMessage && (<Snackbar
+        open={openToastMessage}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        TransitionComponent={SlideTransition}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <Alert severity={typeToastMessage} sx={{ width: '100%' }}>
+          {titleToastMessage}
+        </Alert>
+      </Snackbar>)}
+  </div>
   );
 };
 
