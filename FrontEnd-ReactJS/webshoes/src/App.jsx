36a4df3080a5300e5721cf83604a6f6203ce798a
Tran Huy Hoang <tranhoang202204@gmail.com>
2024-11-09 12:33:54 +0700

    Config public routes, private routes, admin routes

--- FrontEnd-ReactJS/webshoes/src/App.jsx
+++ FrontEnd-ReactJS/webshoes/src/App.jsx
@@ -1,73 +1,74 @@
-import React, { useState } from 'react';
-import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
-import AdminHome from './admin/pages/AdminHome/AdminHome.jsx';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useNavigate, Navigate, BrowserRouter } from "react-router-dom";
 import AdminLayout from './layouts/admin/AdminLayout.jsx';
 import UserLayout from './layouts/user/UserLayout.jsx';
-import UserHome from './user/pages/UserHome/UserHome.jsx';
-import Users from './admin/pages/Users/Users.jsx';
-import User from "./admin/pages/User/User.jsx";
-import AdminProducts from "./admin/pages/AdminProducts/AdminProducts.jsx";
-import AdminProduct from "./admin/pages/AdminProduct/AdminProduct.jsx";
-import Orders from "./admin/pages/Orders/Orders.jsx"
-import AdminCalendar from "./admin/pages/Calendar/Calendar.jsx"
-import PaymentPage from './user/pages/PaymentPage/PaymentPage.jsx';
-import ProductDetailPage from './user/pages/ProductDetailPage/ProductDetailPage.jsx';
-import ProfilePage from './user/pages/ProfilePage/ProfilePage.jsx';
-import './App.scss';
+import { publicRoutes, privateRoutes, adminRoutes } from "./routes/routes.js";
+import PrivateAdminRoutes from "./user/components/PrivateRoute/PrivateAdminRoutes.js";
+import PrivateUserRoutes from "./user/components/PrivateRoute/PrivateUserRoutes.js";
 
-// Tạo router cho ứng dụng
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <UserLayout />,
-    children: [
-      {
-        path: "/",
-        element: <UserHome />, // Thêm route cho SignInComponent
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <AdminLayout />,
-    children: [
-      {
-        path: "",
-      element: <AdminHome />, // Giả định AdminClient đã được định nghĩa
-      },
-      {
-        path: "/admin/users",
-        element: <Users />,
-      },
-      {
-        path: "/admin/users/:id",
-        element: <User />,
-      },
-      {
-        path: "/admin/products",
-        element: <AdminProducts />,
-      },
-      {
-        path: "/admin/products/:id",
-        element: <AdminProduct />,
-      },
-      {
-        path: "/admin/orders/",
-        element: <Orders />,
-      },
-      {
-        path: "/admin/calendar/",
-        element: <AdminCalendar />,
-      },
-    ],
-  },
-]);
-
-// Component App
 function App() {
-  return (
-    <RouterProvider router={router} />
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+
+                    <Route element={<UserLayout/>}>
+                        {publicRoutes.map((route, index) => {
+                            const Page = route.element;
+                            return (
+                                <Route
+                                    key={index}
+                                    path={route.path}
+                                    element={<Page />}
+                                >
+                                    {route?.children?.map((childrenRoute, index) => {
+                                        return (
+                                            <Route
+                                            key={index}
+                                            path={childrenRoute.path}
+                                            element={childrenRoute.element}
+                                        />
+                                        )
+                                    })}
+                                </Route>
+                            );
+                        })}
+                    </Route>
+
+                    <Route element={<PrivateUserRoutes/>}>
+                        <Route element={<UserLayout/>}>
+                            {privateRoutes.map((route,index) => {
+                                const Page = route.element
+                                return (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        element={<Page/>}
+                                    />
+                                )
+                            })}
+                        </Route>
+                    </Route>
+
+                    <Route element={<PrivateAdminRoutes/>}>
+                        <Route element={<AdminLayout/>}>
+                            {adminRoutes.map((route,index) => {
+                                const Page = route.element
+                                return (
+                                    <Route
+                                        key={index}
+                                        path={route.path}
+                                        element={<Page/>}
+                                    />
+                                )
+                            })}
+                        </Route>
+                    </Route>
+
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App;
+export default App
